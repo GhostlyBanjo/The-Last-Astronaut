9f3e89ab85c3e5619a8dc09093cf81b0a9300f36
GhostlyBanjo <fdavis.3.klein@gmail.com>
2015-05-21 12:59:10 -0500

    Added project Files
    Whoop!

---
+++ Assets/RadSource.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RadSource : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void onCollisionEnter(Collision c){
+		GameObject g = c.gameObject;
+		Renderer r = g.GetComponent<Renderer> ();
+		r.material.SetColor("_SpecColor",Color.green);
+		
+	}
+
+	void onCollisionExit(Collision c){
+		GameObject g = c.gameObject;
+		Renderer r = g.GetComponent<Renderer> ();
+			r.material.SetColor("_SpecColor",Color.red);
+
+	}
+}
---
+++ Assets/WorldObject.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+			if(Input.GetKeyDown(KeyCode.RightArrow)){
+			this.transform.position+= new Vector3(-1,0,0);}
+		 if(Input.GetKeyDown(KeyCode.LeftArrow))
+		   this.transform.position+= new Vector3(1,0,0);
+	
+	}
+	void onCollisionEnter(Collision c){
+		GameObject g = c.gameObject;
+		Renderer r = g.GetComponent<Renderer> ();
+		r.material.SetColor("_SpecColor",Color.green);
+		
+	}
+	
+	void onCollisionExit(Collision c){
+		GameObject g = c.gameObject;
+		Renderer r = this.GetComponent<Renderer> ();
+		r.material.SetColor("_SpecColor",Color.red);
+		
+	}
+}
